1fc18b00cc90b9e72a54764c5000e597418a4aa5
mh <qualitynice@hotmail.com>
2014-04-16 19:48:01 -0400

    Heroku refresh

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140416150523) do
+ActiveRecord::Schema.define(version: 20140416233959) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
